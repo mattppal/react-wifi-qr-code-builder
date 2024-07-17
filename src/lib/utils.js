ff80cb3dc4f119a3da0ced8636d4fdbaec18618d
Matt <59450879+mattppal@users.noreply.github.com>
2024-07-17 09:18:38 -0700

    small updates

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,27 +5,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
-
-
-export function GenPDF(qrcode, ssid) {
-  const SaveFile = () => {
-    const content = qrcode;
-    
-    const blob = new Blob([content], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    
-    const link = document.createElement('a');
-
-    link.href = url;
-    link.download = `{ssid}.pdf`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-  };
-}
-
-
-
-
-export default GenPDF;
